548e750280c20fb4227cca0f45871b88e2428642
hirotaka.iwanaga <iwanaga1623@gmail.com>
2020-03-04 01:19:02 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,19 @@
-class TasksController < ApplicationController
     # Rubyメソッドを使用してSQLを操作する
     # Controller : HTTPリクエストの処理を担当; リクエストに対応した、Modelの取得・作成、保存
     # HTTPリクエスト　Router => Controller: アクション
     # View: Controller が、レスポンスとして返すWebページ; Controllerによって適切なModelがセットされる
+
+class TasksController < ApplicationController
+    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    # before_action で実行するアクションを指定し、各アクションが実行される前に指定する事ができる
     
     def index
         @tasks = Task.all # .all => 全てのレコードを取得する
     end
     
     def show
-        @task = Task.find(params[:id])# idを指定して検索
+        # @task = Task.find(params[:id])# idを指定して検索
+         set_task
     end
     
     def new
@@ -29,12 +33,14 @@ class TasksController < ApplicationController
     end
 
     def edit
-        @task = Task.find(params[:id])
+        # @task = Task.find(params[:id])
+        set_task
     end
 
     def update
         def update
-            @task = Task.find(params[:id])
+            # @task = Task.find(params[:id])
+            set_task
             if @task.update(task_params)
                 flash[:success] = 'Task は正常に更新されました'
                 redirect_to @task
@@ -46,13 +52,18 @@ class TasksController < ApplicationController
     end
 
     def destroy
-        @tasks = Task.find(params[:id])
+        # @tasks = Task.find(params[:id])
+        set_task
         @tasks.destroy
         flash[:success] = 'Task は正常に削除されました'
         redirect_to tasks_url
     end
     
     private
+    
+    def set_task
+        @task = Task.find(params[:id])
+    end
 
   # Strong Parameter
     def task_params

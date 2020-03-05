9ce273607329dc13d3135630ea40436c2892d041
hirotaka.iwanaga <iwanaga1623@gmail.com>
2020-03-05 10:25:05 +0000

    add-Status

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -58,16 +58,12 @@ class TasksController < ApplicationController
     
     private
     
-#    def task_params
-#      params.require(:task).permit(:content, :status)
-#    end
-
     def set_task
       @task = Task.find(params[:id])
     end
 
   # Strong Parameter
     def task_params
-      params.require(:task).permit(:content)
+      params.require(:task).permit(:content, :status)
     end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -6,5 +6,5 @@
 
 class Task < ApplicationRecord
     validates :content, presence: true, length: { maximum: 255 }
-    validates :status, presence: true, length: { maximun: 10 }
+    validates :status, presence: true, length: { maximum: 10 }
 end

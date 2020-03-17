9ee981d143825e48e083eb2c5c3ffbd0d1e69b3a
hirotaka.iwanaga <iwanaga1623@gmail.com>
2020-03-17 13:12:30 +0000

    push

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,10 +2,12 @@ class TasksController < ApplicationController
     before_action :require_user_logged_in
 
     def create
-      @task = Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
+#      p 'ここを見ろ！'
+#      p @task
       if @task.save# => レコードの作成
         flash[:success] = 'Task が正常に投稿されました'
-        redirect_to @task
+        redirect_to root_path
       else
         flash.now[:danger] = 'Task が投稿されませんでした'
         render :new
@@ -13,9 +15,14 @@ class TasksController < ApplicationController
     end
 
     def destroy
+#      p params
+#      p 'ここを見ろ！'
+#      p params[:id]
+#      p params[:action]
+      @task = Task.find(params[:id])
       @task.destroy
       flash[:success] = 'Task は正常に削除されました'
-      redirect_to tasks_url
+      redirect_to root_path
     end
     
     private

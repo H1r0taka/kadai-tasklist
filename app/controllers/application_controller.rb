3f39a13c4c82c25c60bebd1f6bc6618bcf31ceb4
hirotaka.iwanaga <iwanaga1623@gmail.com>
2020-03-20 09:14:58 +0000

    p

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :require_user_logged_in, only: [:index]
+  before_action :require_user_logged_in, only: [:index, :show, :edit]
   include SessionsHelper
 
   private

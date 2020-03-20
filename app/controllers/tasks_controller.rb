0651907335159c8a6b976ab2a6678974d00314e1
hirotaka.iwanaga <iwanaga1623@gmail.com>
2020-03-20 10:50:31 +0000

    p

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
     before_action :require_user_logged_in
-    
+    before_action :correct_user
     def show
       @task = Task.find(params[:id])
     end
